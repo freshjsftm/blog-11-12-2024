a5e7376773897f63d28941c88b33feb69848985f
Olena Zdanovska <freshjs.ftm@gmail.com>
2025-01-22 17:36:56 +0200

    Fix pagination at SearchResultPage

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -4,7 +4,7 @@ import { mdiChevronLeft, mdiChevronRight } from '@mdi/js';
 import styles from './Pagination.module.scss';
 
 const Pagination = (props) => {
-  const { page, setPage } = props;
+  const { page, setPage, total, limit } = props;
   const setPrevPage = () => {
     if (page > 1) {
       setPage((prev) => prev - 1);
@@ -17,13 +17,17 @@ const Pagination = (props) => {
   };
   return (
     <div className={styles.pagination}>
-      <span onClick={setPrevPage}>
-        <Icon size={2} path={mdiChevronLeft} />
-      </span>
+      {page > 1 && (
+        <span onClick={setPrevPage}>
+          <Icon size={2} path={mdiChevronLeft} />
+        </span>
+      )}
       <span className={styles.page}>{page}</span>
-      <span onClick={setNextPage}>
-        <Icon size={2} path={mdiChevronRight} />
-      </span>
+      {total - limit * page > 0 && (
+        <span onClick={setNextPage}>
+          <Icon size={2} path={mdiChevronRight} />
+        </span>
+      )}
     </div>
   );
 };
@@ -31,6 +35,8 @@ const Pagination = (props) => {
 Pagination.propTypes = {
   page: PropTypes.number,
   setPage: PropTypes.func,
+  total: PropTypes.number,
+  limit: PropTypes.number,
 };
 
 export default Pagination;
--- src/pages/SearchResultPage.jsx
+++ src/pages/SearchResultPage.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import { useSearchParams } from 'react-router-dom';
 import PostsSearchList from '../components/PostsList/PostsSearchList';
 import Pagination from '../components/Pagination/Pagination';
@@ -10,6 +11,7 @@ const SearchResultPage = () => {
   const [page, setPage] = useState(1);
   const limitPosts = CONSTANTS.LIMITS_POSTS.at(2); //5
   const skip = (page - 1) * limitPosts;
+  const { total } = useSelector((state) => state.posts);
 
   useEffect(() => {
     setPage(1);
@@ -19,8 +21,12 @@ const SearchResultPage = () => {
     <div>
       <h1>Search result by: {q}</h1>
       <section>
-        <PostsSearchList q={q} limit={limitPosts} skip={skip}/>
-        <Pagination page={page} setPage={setPage} />
+        <PostsSearchList q={q} limit={limitPosts} skip={skip} />
+        {total > 0 ? (
+          <Pagination page={page} setPage={setPage} total={total} limit={limitPosts}/>
+        ) : (
+          <p>posts not found</p>
+        )}
       </section>
     </div>
   );
