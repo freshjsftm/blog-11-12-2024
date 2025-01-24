5958401cb00cd9dbbc5bcfa7cd4423897faf287b
Olena Zdanovska <freshjs.ftm@gmail.com>
2025-01-24 16:38:03 +0200

    Refactor slice

--- src/store/postsSlice.js
+++ src/store/postsSlice.js
@@ -94,6 +94,19 @@ export const getAllCommentsByPostAsync = createAsyncThunk(
   }
 );
 
+const setPending = (state) => {
+  state.isPending = true;
+};
+const setRejected = (state, action) => {
+  state.isPending = false;
+  state.error = action.payload;
+};
+const setFulfilled = (state, action) => {
+  state.isPending = false;
+  state.posts = action.payload.posts;
+  state.total = action.payload.total;
+};
+
 const postsSlice = createSlice({
   name: 'posts',
   initialState: {
@@ -109,89 +122,42 @@ const postsSlice = createSlice({
   },
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(searchPostsAsync.pending, (state) => {
-      state.isPending = true;
-    });
-    builder.addCase(searchPostsAsync.fulfilled, (state, action) => {
-      state.isPending = false;
-      state.posts = action.payload.posts;
-      state.total = action.payload.total;
-    });
-    builder.addCase(searchPostsAsync.rejected, (state, action) => {
-      state.isPending = false;
-      state.error = action.payload;
-    });
+    builder.addCase(searchPostsAsync.pending, setPending);
+    builder.addCase(searchPostsAsync.fulfilled, setFulfilled);
+    builder.addCase(searchPostsAsync.rejected, setRejected);
 
-    builder.addCase(getAllPostsByTagAsync.pending, (state) => {
-      state.isPending = true;
-    });
-    builder.addCase(getAllPostsByTagAsync.fulfilled, (state, action) => {
-      state.isPending = false;
-      state.posts = action.payload.posts;
-      state.total = action.payload.total;
-    });
-    builder.addCase(getAllPostsByTagAsync.rejected, (state, action) => {
-      state.isPending = false;
-      state.error = action.payload;
-    });
+    builder.addCase(getAllPostsByTagAsync.pending, setPending);
+    builder.addCase(getAllPostsByTagAsync.fulfilled, setFulfilled);
+    builder.addCase(getAllPostsByTagAsync.rejected, setRejected);
 
-    builder.addCase(getAllTagsAsync.pending, (state) => {
-      state.isPending = true;
-    });
+    builder.addCase(getAllTagsAsync.pending, setPending);
     builder.addCase(getAllTagsAsync.fulfilled, (state, action) => {
       state.isPending = false;
       state.tags = action.payload;
     });
-    builder.addCase(getAllTagsAsync.rejected, (state, action) => {
-      state.isPending = false;
-      state.error = action.payload;
-    });
-    builder.addCase(getAllPostsByUserAsync.pending, (state) => {
-      state.isPending = true;
-    });
+    builder.addCase(getAllTagsAsync.rejected, setRejected);
+    builder.addCase(getAllPostsByUserAsync.pending, setPending);
     builder.addCase(getAllPostsByUserAsync.fulfilled, (state, action) => {
       state.isPending = false;
       state.postsByUser = action.payload;
     });
-    builder.addCase(getAllPostsByUserAsync.rejected, (state, action) => {
-      state.isPending = false;
-      state.error = action.payload;
-    });
+    builder.addCase(getAllPostsByUserAsync.rejected, setRejected);
 
-    builder.addCase(getAllPostsAsync.pending, (state) => {
-      state.isPending = true;
-    });
-    builder.addCase(getAllPostsAsync.fulfilled, (state, action) => {
-      state.isPending = false;
-      state.posts = action.payload.posts;
-      state.total = action.payload.total;
-    });
-    builder.addCase(getAllPostsAsync.rejected, (state, action) => {
-      state.isPending = false;
-      state.error = action.payload;
-    });
-    builder.addCase(getOnePostAsync.pending, (state) => {
-      state.isPending = true;
-    });
+    builder.addCase(getAllPostsAsync.pending, setPending);
+    builder.addCase(getAllPostsAsync.fulfilled, setFulfilled);
+    builder.addCase(getAllPostsAsync.rejected, setRejected);
+    builder.addCase(getOnePostAsync.pending, setPending);
     builder.addCase(getOnePostAsync.fulfilled, (state, action) => {
       state.isPending = false;
       state.selectedPost = action.payload;
     });
-    builder.addCase(getOnePostAsync.rejected, (state, action) => {
-      state.isPending = false;
-      state.error = action.payload;
-    });
-    builder.addCase(getAllCommentsByPostAsync.pending, (state) => {
-      state.isPending = true;
-    });
+    builder.addCase(getOnePostAsync.rejected, setRejected);
+    builder.addCase(getAllCommentsByPostAsync.pending, setPending);
     builder.addCase(getAllCommentsByPostAsync.fulfilled, (state, action) => {
       state.isPending = false;
       state.comments = action.payload;
     });
-    builder.addCase(getAllCommentsByPostAsync.rejected, (state, action) => {
-      state.isPending = false;
-      state.error = action.payload;
-    });
+    builder.addCase(getAllCommentsByPostAsync.rejected, setRejected);
   },
 });
 
